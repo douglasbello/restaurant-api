9c76278e1223f2337670d2356003d75cb4cc24d7
douglasbello <douglasbelloalv1@outlook.com>
2023-08-12 16:33:57 -0300

    first commit

---
+++ src/test/java/br/com/douglasbello/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.douglasbello.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
